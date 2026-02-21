96cd4a0a3996e7e9fe5403cc0750dde9c1a956dc
arpan7sarkar <arpansakar@gmail.com>
2026-02-21 10:20:31 +0530

    added login api

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -69,7 +69,52 @@ const register = async (req, res) => {
 const login = async (req, res) => {
     
     try {
-        
+        const {username , email, password} = req.body;
+
+        if((!username && !email) || !password){
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required"
+            })
+        }
+
+        const user = await userModel.findOne({
+            $or: [
+                { email },
+                { username }
+            ]
+        })
+        if(!user){
+            return res.status(404).json({
+                success: false,
+                message: "User not found"
+            })
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if(!isPasswordValid){
+            return res.status(401).json({
+                success: false,
+                message: "Invalid credentials"
+            })
+        }
+
+        const token = jwt.sign({
+            id: user._id
+        }, process.env.JWT_SECRET, {
+            expiresIn: "1d"
+        })
+        res.cookie("token", token);
+        res.status(200).json({
+            success: true,
+            message: "User logged in successfully",
+            user:{
+                username:user.username,
+                email:user.email,
+                bio:user.bio,
+                profileImage:user.profileImage
+            }
+        })
     } catch (error) {
         res.status(500),json({
             success: false,
@@ -77,4 +122,4 @@ const login = async (req, res) => {
         })
     }
 }
-module.exports = { register }
+module.exports = { register , login }
